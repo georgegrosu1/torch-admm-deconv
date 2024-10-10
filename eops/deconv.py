9e302a6eb4d5660b5c59c91c1b5b368c89dc9f96
georgegrosu1 <george.grosu.florin@gmail.com>
2024-10-10 21:22:53 +0300

    Pytorch functional implementation of ADMM deconvolution

--- eops/deconv.py
+++ eops/deconv.py
@@ -5,7 +5,7 @@ from typing import Tuple
 
 
 def torch_abs2(x: torch.Tensor) -> torch.Tensor:
-    return torch.pow(torch.abs(x), 2)
+    return torch.abs(x) ** 2
 
 
 def hard_thresh(x: torch.Tensor, tau: float) -> torch.Tensor:
@@ -16,12 +16,12 @@ def soft_thresh(x: torch.Tensor, tau: float) -> torch.Tensor:
     return torch.sign(x) * torch.maximum(torch.abs(x)-tau, torch.tensor([0]))
 
 
-def block_thresh(x: torch.Tensor, tau: float) -> torch.Tensor:
+def block_thresh(x: torch.Tensor, tau: torch.Tensor) -> torch.Tensor:
     return torch.maximum(1 - tau / pixelnorm(x), torch.tensor([0])) * x
 
 
 def pixelnorm(x: torch.Tensor) -> torch.Tensor:
-    return torch.sqrt(torch.sum(torch.pow(x, 2), (2, 3)))
+    return torch.sqrt(torch.sum(x ** 2, (0, 1)))
 
 
 def identity(x: torch.Tensor) -> torch.Tensor:
@@ -39,75 +39,79 @@ def fft_admm_tv(xin: torch.Tensor,
                 iso: bool=False,
                 maxit: int=100) -> torch.Tensor:
 
-    # make x from (batch, ch, h, w) -> (h, w, batch, ch)
-    xin = xin.permute(2,3,0,1)
-    M, N, B, P = xin.shape
+    B, C, H_im, W_im = xin.shape
 
     tau = lmbd / rho
 
     if kern.numel() == 0:
-        k_kern = kern
-        sigma = torch.tensor([1])
+        sigma = torch.tensor([1], dtype=xin.dtype, device=xin.device)
     else:
-        k_kern = torch.constant_pad_nd(kern, (0, M-kern.shape[0], 0, N-kern.shape[1]))
-        sigma = torch.fft.rfftn(k_kern)
+        sigma = torch.fft.rfftn(kern, s=(H_im,W_im), dim=(2,3))
 
-    dx_base = torch.tensor([[1, 0], [-1, 0]])
-    dy_base = torch.tensor([[1, -1], [0, 0]])
-    dx_filter = torch.constant_pad_nd(dx_base, (0, M - 2, 0, N - 2))
-    dy_filter = torch.constant_pad_nd(dy_base, (0, M - 2, 0, N - 2))
+    dx_base = torch.tensor([[[[0, 0], [-1, 1]]]], dtype=xin.dtype, device=xin.device)
+    dy_base = torch.tensor([[[[0, -1], [0, 1]]]], dtype=xin.dtype, device=xin.device)
 
-    delta_dx = torch.fft.rfftn(dx_filter)
-    delta_dy = torch.fft.rfftn(dy_filter)
+    delta_dx = torch.fft.rfftn(dx_base, s=(1,1,H_im,W_im))
+    delta_dy = torch.fft.rfftn(dy_base, s=(1,1,H_im,W_im))
 
     freq_c = 1 / (torch_abs2(sigma) + rho * (torch_abs2(delta_dx) + torch_abs2(delta_dy)))
 
     thresh = block_thresh if iso else soft_thresh
 
-    x = torch.zeros((M, N, B, P))
-    dx_k = torch.zeros((M, N, 2*B, P))
-    z = torch.zeros((M, N, 2*B, P))
-    u = torch.zeros((M, N, 2*B, P))
+    x = torch.zeros_like(xin, dtype=xin.dtype, device=xin.device)
 
-    w_normal_1 = torch.tensor([[1, -1], [0, 0]])
-    w_normal_2 = torch.tensor([[1, 0], [-1, 0]])
-    w_normal = torch.cat((w_normal_1[:,:,torch.newaxis,torch.newaxis],
-                          w_normal_2[:,:,torch.newaxis,torch.newaxis]), 3)
-    w_tr_1 = torch.tensor([[0, 0], [-1, 1]])
-    w_tr_2 = torch.tensor([[0, -1], [0, 1]])
-    w_tr = torch.cat((w_tr_1[:,:,torch.newaxis,torch.newaxis],
-                      w_tr_2[:,:,torch.newaxis,torch.newaxis]), 2)
+    z_x = torch.zeros_like(xin, dtype=xin.dtype, device=xin.device)
+    z_y = torch.zeros_like(xin, dtype=xin.dtype, device=xin.device)
+
+    u_x = torch.zeros_like(xin, dtype=xin.dtype, device=xin.device)
+    u_y = torch.zeros_like(xin, dtype=xin.dtype, device=xin.device)
+
+    w_x_base = dx_base.repeat((C, 1, 1, 1))
+    w_y_base = dy_base.repeat((C, 1, 1, 1))
+    w_x_conj = dx_base.flip(2,3).repeat((C, 1, 1, 1))
+    w_y_conj = dy_base.flip(2,3).repeat((C, 1, 1, 1))
+
+    def Dx(a: torch.Tensor) -> torch.Tensor:
+        return conv_circular(a, w_x_base, pads=(1, 0, 1, 0), groups=C)
+
+    def Dy(a: torch.Tensor) -> torch.Tensor:
+        return conv_circular(a, w_y_base, pads=(1, 0, 1, 0), groups=C)
+
+    def Dx_t(a: torch.Tensor) -> torch.Tensor:
+        return conv_circular(a, w_x_conj, pads=(0, 1, 0, 1), groups=C)
+
+    def Dy_t(a: torch.Tensor) -> torch.Tensor:
+        return conv_circular(a, w_y_conj, pads=(0, 1, 0, 1), groups=C)
 
     if kern.numel() == 0:
-        H = identity
         H_t = identity
     else:
-        k_kern = k_kern.repeat((1,1,1,B))
-        k_kern_t = k_kern.flip((0,1))
-        padup = torch.ceil(torch.tensor(k_kern.size(0) - 1) / 2)
-        paddown = torch.floor(torch.tensor(k_kern.size(0) - 1) / 2)
-        padleft = torch.ceil(torch.tensor(k_kern.size(1) - 1) / 2)
-        padright = torch.floor(torch.tensor(k_kern.size(1) - 1) / 2)
+        k_kern_t = kern.flip((2,3)).repeat((C,1,1,1))
+        padup = torch.ceil(torch.tensor(kern.size(2) - 1, device=xin.device) / 2)
+        paddown = torch.floor(torch.tensor(kern.size(2) - 1, device=xin.device) / 2)
+        padleft = torch.ceil(torch.tensor(kern.size(3) - 1, device=xin.device) / 2)
+        padright = torch.floor(torch.tensor(kern.size(3) - 1, device=xin.device) / 2)
 
-        pad1 = (padup, paddown, padleft, padright)
-        pad2 = (paddown, padup, padright, padleft)
-
-        def hnorm(x: torch.Tensor) -> torch.Tensor:
-            return conv_circular(x, k_kern, pad1, groups=B)
+        pad1 = (int(padup), int(paddown), int(padleft), int(padright))
+        pad2 = (int(paddown), int(padup), int(padright), int(padleft))
 
         def htran(x: torch.Tensor) -> torch.Tensor:
-            return conv_circular(x, k_kern_t, pad2, groups=B)
+            return conv_circular(x, k_kern_t, pad2, groups=C)
 
-        H = hnorm
         H_t = htran
 
-        for _ in torch.range(0, maxit):
-            rfft_term = torch.fft.rfftn(H_t(xin) + rho * conv_circular(z-u, w_tr, (0,1,0,1), B), dim=(0,1))
-            x = torch.fft.irfftn(freq_c * rfft_term, (0,1))
-            dx_k = conv_circular(x, w_normal, (1,0,1,0), B)
-            z = thresh(dx_k + u, tau)
-            u += dx_k - z
+    for _ in torch.arange(0, maxit):
+        rfft_term = torch.fft.rfftn(H_t(xin) + rho * (Dx_t(z_x-u_x) + Dy_t(z_y-u_y)), dim=(2,3))
 
-        x = x.permute(2,3,0,1)
+        x = torch.fft.irfftn(freq_c * rfft_term, (H_im, W_im), dim=(2,3))
 
-        return x
+        dx_k = Dx(x)
+        dy_k = Dy(x)
+
+        z_x = thresh(dx_k + u_x, tau)
+        z_y = thresh(dy_k + u_y, tau)
+
+        u_x += dx_k - z_x
+        u_y += dy_k - z_y
+
+    return x
