f565d542a32fa06b558d52caf4a15adc80d943a2
georgegrosu1 <george.grosu.florin@gmail.com>
2024-11-18 13:50:41 +0200

    Updates for dataset

---
+++ make_blur_dset.py
@@ -0,0 +1,110 @@
+import argparse
+from tqdm import tqdm
+from pathlib import Path
+
+from utils.dset_utils import *
+
+
+def get_train_test_txts(orig_p: Path) -> Tuple[List, List]:
+    train_txts = list(orig_p.glob('*train.txt'))
+    test_txts = list(orig_p.glob('*test.txt'))
+    return train_txts, test_txts
+
+
+def process_x_y_ims(x_ims: List, y_ims: List, min_stdv, max_stdv, sdir_x, sdir_y) -> None:
+    for x_p, y_p in tqdm(zip(x_ims, y_ims), total=len(y_ims)):
+        im_hash = get_rand_uuid()
+        imx, imy = cv2.imread(str(x_p), cv2.IMREAD_COLOR), cv2.imread(str(y_p), cv2.IMREAD_COLOR)
+        if max_stdv > min_stdv >= 0:
+            r_stdv = np.random.randint(min_stdv, max_stdv)
+            imx = add_noise_gaussian(imx, stdv=r_stdv)
+        else:
+            r_stdv = 0
+        imx_p, imy_p = sdir_x / f'{im_hash}_awgn-{r_stdv}.png', sdir_y / f'{im_hash}.png'
+        cv2.imwrite(imx_p, imx)
+        cv2.imwrite(imy_p, imy)
+
+
+def make_realblur_dset(orig: str,
+                       save_dir_train_x: Path, save_dir_train_y: Path,
+                       save_dir_test_x: Path, save_dir_test_y: Path,
+                       min_noise_std: int, max_noise_std: int) -> None:
+
+    train_txts, test_txts = get_train_test_txts(Path(orig))
+    # Process train
+    train_1_x, train_1_y = get_dset_im_paths(train_txts[0])
+    train_2_x, train_2_y = get_dset_im_paths(test_txts[1])
+    train_x, train_y = train_1_x + train_2_x, train_1_y + train_2_y
+    print('\n\nProcessing train data')
+    process_x_y_ims(train_x, train_y, min_noise_std, max_noise_std, save_dir_train_x, save_dir_train_y)
+
+    # Process test
+    test_1_x, test_1_y = get_dset_im_paths(test_txts[0])
+    test_2_x, test_2_y = get_dset_im_paths(test_txts[1])
+    test_x, test_y = test_1_x + test_2_x, test_1_y + test_2_y
+    print('\n\nProcessing test data')
+    process_x_y_ims(test_x, test_y, min_noise_std, max_noise_std, save_dir_test_x, save_dir_test_y)
+
+
+def get_gopro_subset_im_paths(setdir: Path, subset: str = 'blur') -> tuple[list[Path], list[Path]]:
+        x_im_paths, y_im_paths = [], []
+        for sdir in setdir.iterdir():
+            x_im_paths += list((sdir / subset).glob('*.png'))
+            y_im_paths += list((sdir / 'sharp').glob('*.png'))
+
+        return x_im_paths, y_im_paths
+
+
+def make_gopro_dset(orig: str,
+                    save_dir_train_x: Path, save_dir_train_y: Path,
+                    save_dir_test_x: Path, save_dir_test_y: Path,
+                    min_noise_std: int, max_noise_std: int) -> None:
+
+    train_dirs = Path(f'{orig}/train')
+    test_dirs = Path(f'{orig}/test')
+    # Process train
+    train_x, train_y = get_gopro_subset_im_paths(train_dirs, subset='blur')
+    print('\n\nProcessing train data')
+    process_x_y_ims(train_x, train_y, min_noise_std, max_noise_std, save_dir_train_x, save_dir_train_y)
+    # Process test
+    test_x, test_y = get_gopro_subset_im_paths(test_dirs, subset='blur')
+    print('\n\nProcessing test data')
+    process_x_y_ims(test_x, test_y, min_noise_std, max_noise_std, save_dir_test_x, save_dir_test_y)
+
+
+def main():
+    args_parser = argparse.ArgumentParser(description='Script to generate dataset with noise and blur')
+    args_parser.add_argument('--dset', '-d', type=str, help='Dataset',
+                             default=r'gopro')
+    args_parser.add_argument('--orig', '-o', type=str, help='Path to RealBlur dir',
+                             default=r'D:/Projects/datasets/GoPro/GOPRO_Large')
+    args_parser.add_argument('--save_dir', '-s', type=str, help='Dir (relative to cwd) to save images',
+                             default=r'D:/Projects/datasets/GoPro/GOPRO_Large/orig_blur')
+    args_parser.add_argument('--min_noise_std', '-m', type=int, help='Minimum std of noise level',
+                             default=15)
+    args_parser.add_argument('--max_noise_std', '-M', type=int, help='Maximum std of noise level',
+                             default=60)
+    args = args_parser.parse_args()
+
+
+
+    save_dir_train = Path(args.save_dir) / f'awgn-{args.min_noise_std}-{args.max_noise_std}' / 'train'
+    save_dir_train_x, save_dir_train_y = save_dir_train / 'x', save_dir_train / 'y'
+    save_dir_train_x.mkdir(parents=True, exist_ok=True)
+    save_dir_train_y.mkdir(parents=True, exist_ok=True)
+
+    save_dir_test = Path(args.save_dir) / f'awgn-{args.min_noise_std}-{args.max_noise_std}' / 'test'
+    save_dir_test_x, save_dir_test_y = save_dir_test / 'x', save_dir_test / 'y'
+    save_dir_test_x.mkdir(parents=True, exist_ok=True)
+    save_dir_test_y.mkdir(parents=True, exist_ok=True)
+
+    if args.dset == 'gopro':
+        make_gopro_dset(args.orig, save_dir_train_x, save_dir_train_y, save_dir_test_x, save_dir_test_y,
+                        args.min_noise_std, args.max_noise_std)
+    elif args.dset == 'realblur':
+        make_realblur_dset(args.orig, save_dir_train_x, save_dir_train_y, save_dir_test_x, save_dir_test_y,
+                           args.min_noise_std, args.max_noise_std)
+
+
+if __name__ == "__main__":
+    main()
