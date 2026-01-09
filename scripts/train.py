10f283a04236115afc2035eaa138124e8958a582
georgegrosu1 <george.grosu.florin@gmail.com>
2026-01-09 15:42:19 +0200

    fix: need flips actually is ok

--- scripts/train.py
+++ scripts/train.py
@@ -60,7 +60,7 @@ def init_training(config_file: str, min_std: int, max_std: int, save_dir: str, m
 
     # Prepare train & eval data loaders
     im_shape = tuple(train_cfg['im_shape'])
-    transforms = [RandCrop(im_shape), Scale()]
+    transforms = [RandCrop(im_shape), Scale(), Flip()]
     if max_std > 0: transforms += [AddAWGN(std_range=(min_std, max_std), both=False)]
     train_dset = ImageDataset(Path(train_cfg['train']['x_path']), Path(train_cfg['train']['y_path']), device=device,
                               transforms=transforms)
